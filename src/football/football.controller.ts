a92a5f2599a5c9ceb22f8420966ab25f95117828
achourali <achourali1999@gmail.com>
2022-04-09 02:09:01 +0100

    get all leagues

--- src/football/football.controller.ts
+++ src/football/football.controller.ts
@@ -1,13 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { Country } from 'src/schemas/country.schema';
+import { League } from 'src/schemas/league.schema';
 import { FootballService } from './football.service';
 
 @Controller('football')
 export class FootballController {
-    constructor(private readonly footballService: FootballService) {}
+  constructor(private readonly footballService: FootballService) {}
 
   @Get('countries')
-  async scrapeCountries(): Promise<Country[]> {
-      return this.footballService.getCountries();
+  async getCountries(): Promise<Country[]> {
+    return this.footballService.getCountries();
+  }
+  @Get('leagues')
+  async getAllLeagues(): Promise<League[]> {
+    let leagues = await this.footballService.getAllLeagues();
+    console.log(leagues[0].country.name)
+    return leagues
   }
 }
--- src/football/football.service.ts
+++ src/football/football.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Country, CountryDocument } from 'src/schemas/country.schema';
 import { Model } from 'mongoose';
+import { League, LeagueDocument } from 'src/schemas/league.schema';
 
 @Injectable()
 export class FootballService {
   constructor(
     @InjectModel(Country.name) private countryModel: Model<CountryDocument>,
+    @InjectModel(League.name) private leagueModel: Model<LeagueDocument>,
   ) {}
 
   async getCountries(): Promise<Country[]> {
     return await this.countryModel.find();
+  }
+
+  async getAllLeagues(): Promise<League[]> {
+    return await this.leagueModel.find().populate('country').populate('sport');
   }
 }

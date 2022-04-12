02acce80eea69f1de808430ec36e65c5d80d38c7
achourali <achourali1999@gmail.com>
2022-04-12 10:27:13 +0100

    enable cors origin:front

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors({origin:"http://localhost:4200"});
   await app.listen(process.env.APP_PORT);
 }
 

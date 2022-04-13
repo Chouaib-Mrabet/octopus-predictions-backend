51d01905bf0e56981ec9fe677b6ad20cc8aea3af
achourali <achourali1999@gmail.com>
2022-04-13 14:54:08 +0100

    remove password from dbBackup.sh

--- dbBackup.sh
+++ dbBackup.sh
@@ -1,2 +1,2 @@
 
-mongodump --uri="mongodb+srv://user:mqwkzUkwQEFlaUTj@octopuspredictions.czilc.mongodb.net/OctopusPredictions" -o /tmp/`date +"backup_%H:%M_%d%b%Y"`
+mongodump --uri="mongodb+srv://user:@octopuspredictions.czilc.mongodb.net/OctopusPredictions" -o /tmp/`date +"backup_%H:%M_%d%b%Y"`
